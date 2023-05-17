1414f01a2d986269da21dacd23063e0bea303523
rahulgowda00 <rahulgowda00777@gmail.com>
2023-05-17 15:09:19 +0530

    committed

--- app/src/main/java/com/example/sms/MainActivity.kt
+++ app/src/main/java/com/example/sms/MainActivity.kt
@@ -30,33 +30,14 @@ class MainActivity : AppCompatActivity() {
         btnsend = findViewById(R.id.btnsend)
 
         btnsend.setOnClickListener {
-<<<<<<< HEAD
-            if (true == true) {
-//                var obj = SmsManager.getDefault()
-                obj.sendTextMessage("9066951700", null, "noob", null, null)
-=======
             try {
                 var obj = SmsManager.getDefault()
                 obj.sendTextMessage(phno.text.toString(), null, smstxt.text.toString(), null, null)
-//                obj.sendTextMessage("9606689635", null, "loser", null, null)
->>>>>>> 6333cbbd9e2a26f20a7c92029365dae980631913
                 Toast.makeText(applicationContext, "SMS Sent!", Toast.LENGTH_LONG).show()
             }
-            else{
-//                var obj = SmsManager.getDefault()
-                obj.sendTextMessage("9066951700", null, "hi", null, null)
-                Toast.makeText(applicationContext, "SMS Sent!", Toast.LENGTH_LONG).show()
+            catch(e: Exception){
+                Toast.makeText(applicationContext, "Error", Toast.LENGTH_LONG).show()
             }
-//            try {
-//                if (true == true) {
-//                    var obj = SmsManager.getDefault()
-//                    obj.sendTextMessage("9036850638", null, "you are ew", null, null)
-//                    Toast.makeText(applicationContext, "SMS Sent!", Toast.LENGTH_LONG).show()
-//                }
-//            }
-//            catch(e: Exception){
-//                Toast.makeText(applicationContext, "Error", Toast.LENGTH_LONG).show()
-//            }
         }
     }
 }
